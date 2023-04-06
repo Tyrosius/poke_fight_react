4f1e631fc2d2182425ddff0b2b34c49f0f4d5833
VerenaSchwachmeyer <Vronus@gmx.de>
2023-04-06 19:26:40 +0200

    added first fetch in App.js

--- pokefight/src/App.js
+++ pokefight/src/App.js
@@ -1,6 +1,23 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [pokemon, setPokemons] = useState();
+
+  const getPokemons = async () => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json;
+      setPokemons(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getPokemons();
+  }, []);
+
   return (
     <div className="App">
       <h1>Hello Poke Fight</h1>
